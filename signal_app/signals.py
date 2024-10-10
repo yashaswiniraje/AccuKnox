823cdfe48a8d66d8e1267dabec004edcc4df675a
yashaswiniraje <yashaswinirajebhosle@gmail.com>
2024-10-10 22:23:43 +0530

    First commit message

--- signal_app/signals.py
+++ signal_app/signals.py
@@ -2,8 +2,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 import time
+import threading
+from django.db import transaction
 
-# Pre-save Signal handler for User model
+#Question-1
+
 @receiver(pre_save, sender=User)
 def pre_save_user_signal(sender, instance, **kwargs):
     print(f"Pre_Save Signal started for user: {instance.username}")
@@ -15,21 +18,67 @@ def pre_save_user_signal(sender, instance, **kwargs):
     print(f"Time taken during Pre_Save: {req_time:.2f} seconds")
     print("Pre_Save signal processing finished.")
 
-# Post-save Signal handler for User model
 @receiver(post_save, sender=User)
 def post_save_user_signal(sender, instance, **kwargs):
     print(f"Post_Save Signal started for user: {instance.username}")
     print("Post_Save signal processing finished.")
 
 
+
+#Question-2
+
+import threading
+
+
+@receiver(post_save, sender=User)
+def user_post_save_handler(sender, instance, **kwargs):
+    print(f"Signal Handler: Started for user: {instance.username}")
+    time.sleep(5)
+    print(f"Signal Handler Thread: {threading.current_thread().name}")
+    print(f"Signal Handler: Completed for user: {instance.username}")
+
+#Question -3
+
+
+@receiver(post_save, sender=User)
+def error_in_signal(sender, instance, created, **kwargs):
+    if created:
+        print(f"Signal triggered for new user: {instance.username}")
+        passlen = len(instance.username)
+
+        with transaction.atomic():
+            if passlen < 6:
+                raise Exception("username is too small")
+
+            print("User Successfully created")
+
+
 """
-#yashaswini is user created in database
+Output:
+
 Pre_Save Signal started for user: yashaswini
 Time taken during Pre_Save: 10.00 seconds
 Pre_Save signal processing finished.
 Post_Save Signal started for user: yashaswini
 Post_Save signal processing finished.
-Time taken during Pre_Save: 10.00 seconds
-Pre_Save signal processing finished.
-
+Signal Handler: Started for user: yashaswini
+Signal Handler Thread: Thread-11 (process_request_thread)
+Signal Handler: Completed for user: yashaswini
+Signal triggered for new user: yashaswini
+User Successfully created
 """
+
+#Task 4-  Topic: Custom Classes in Python
+
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self.values = [{'length': length}, {'width': width}]
+    
+    def __iter__(self):
+        return iter(self.values)
+rect = Rectangle(10, 5)
+
+for item in rect:
+    print(item)
+
+
